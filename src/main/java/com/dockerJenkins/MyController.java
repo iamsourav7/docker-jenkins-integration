e9b3a23db5a8b9a2f1b7003e7450af86317cbdc3
iamsourav7 <90547012+iamsourav7@users.noreply.github.com>
2023-07-17 13:39:28 +0530

    Update MyController.java

--- src/main/java/com/dockerJenkins/MyController.java
+++ src/main/java/com/dockerJenkins/MyController.java
@@ -3,5 +3,7 @@ package com.dockerJenkins;
 public class MyController {
     public static void main(String[] args) {
         System.out.println("sourav");
+                System.out.println("maha");
+
     }
 }
